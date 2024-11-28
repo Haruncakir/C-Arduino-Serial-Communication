256a477c07ff0aceaba2bb548376ffa0190206e7
Harun <ahmetharuncakir5600@gmail.com>
2024-11-28 08:15:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,38 +1,137 @@
+/**
+ * @file main.c
+ * @brief LED control program using serial communication
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "serial_functions.h"
 
-int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    printf("Usage: %s /dev/ttyACM0\n", argv[0]);
-    return 1;
-  }
+#if defined(__linux__)
+    #include <unistd.h>
+    #define SLEEP_MS(ms) usleep((ms) * 1000)
+#elif defined(_WIN32)
+    #include <windows.h>
+    #define SLEEP_MS(ms) Sleep(ms)
+#endif
 
-  const char* portName = argv[1];
-  int serialPort = open_serial_port(portName);
+#define COMMAND_BUFFER_SIZE 2
+#define READ_BUFFER_SIZE 256
+#define SERIAL_DELAY_MS 100
 
-  for(;;) {
-    char command;
-    printf("Select an option:\n");
-    printf("1-Turn on Red Led.\n");
-    printf("2-Turn on Yellow Led.\n");
-    printf("3-Turn on Blue Led.\n");
-    printf("4-Turn off the Leds.\n");
-    printf("5-Quit.\n");
-    printf(">");
-    scanf(" %c", &command);
+static const char* const MENU_OPTIONS[] = {
+    "1-Turn on Red Led",
+    "2-Turn on Yellow Led",
+    "3-Turn on Blue Led",
+    "4-Turn off the Leds",
+    "5-Quit"
+};
 
+static void clear_input_buffer(void) {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+static void display_menu(void) {
+    printf("\nSelect an option:\n");
+    for (size_t i = 0; i < sizeof(MENU_OPTIONS) / sizeof(MENU_OPTIONS[0]); i++) {
+        printf("%s\n", MENU_OPTIONS[i]);
+    }
+    printf("> ");
+    fflush(stdout);
+}
+
+static int process_command(serial_handle_t serial_port, char command) {
+    size_t bytes_written, bytes_read;
+    char read_buffer[READ_BUFFER_SIZE];
+    
+    /* Validate command range */
     if (command == '5') {
-      close_serial_port(serialPort);
-      return 0;
+        return 0;  /* Exit requested */
+    }
+    
+    if (command < '1' || command > '4') {
+        printf("Invalid option. Please select 1-4 or 5 to quit.\n");
+        return 1;
     }
 
-    if (command >= '1' && command <= '4') {
-      serial_write(serialPort, &command);
-      usleep(100000); // Wait for 100ms to allow the Arduino to process the command
-      serial_read(serialPort);
-    } else {
-      printf("Invalid option. Please select 1-4 or 5 to quit.\n");
+    /* Send command to device */
+    if (serial_write(serial_port, &command, 1, &bytes_written) != SERIAL_SUCCESS || bytes_written != 1) {
+        fprintf(stderr, "Failed to send command to device\n");
+        return -1;
     }
-  }
-  return 0;
+
+    /* Wait for device to process command */
+    SLEEP_MS(SERIAL_DELAY_MS);
+
+    /* Read response */
+    if (serial_read(serial_port, read_buffer, READ_BUFFER_SIZE - 1, &bytes_read) != SERIAL_SUCCESS) {
+        fprintf(stderr, "Failed to read device response\n");
+        return -1;
+    }
+
+    if (bytes_read > 0) {
+        read_buffer[bytes_read] = '\0';
+        printf("Arduino response: %s", read_buffer);
+    }
+
+    return 1;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <serial_port>\n", argv[0]);
+        fprintf(stderr, "Example: %s /dev/ttyACM0\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    /* Configure serial port */
+    struct serial_config_s config = {
+        .baud_rate = 9600,
+        .data_bits = 8,
+        .stop_bits = 1,
+        .parity = 0
+    };
+
+    /* Open serial port */
+    serial_handle_t serial_port = serial_open(argv[1], &config);
+    if (serial_port == SERIAL_INVALID_HANDLE) {
+        fprintf(stderr, "Error: Unable to open serial port %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    printf("Connected to %s\n", argv[1]);
+
+    /* Main program loop */
+    int running = 1;
+    char command;
+
+    while (running) {
+        display_menu();
+        
+        if (scanf(" %c", &command) != 1) {
+            clear_input_buffer();
+            continue;
+        }
+        clear_input_buffer();
+
+        int result = process_command(serial_port, command);
+        if (result < 0) {
+            /* Error occurred */
+            fprintf(stderr, "Error during communication with device\n");
+            break;
+        } else if (result == 0) {
+            /* Exit requested */
+            running = 0;
+        }
+    }
+
+    /* Cleanup */
+    if (serial_close(serial_port) != SERIAL_SUCCESS) {
+        fprintf(stderr, "Warning: Error while closing serial port\n");
+    }
+
+    printf("Program terminated.\n");
+    return EXIT_SUCCESS;
 }
